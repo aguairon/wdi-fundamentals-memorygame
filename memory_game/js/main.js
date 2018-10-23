8fc25ca90de8c99fd15396a19310db3f0be69add
Begona Fernandez <boadicea1979@yahoo.es>
2018-10-23 14:56:25 +0100

    web dev fundamentals assignment part 8

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,17 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if(cardsInPlay.length === 2) {
+	console.log("User flipped " + cardsInPlay[0]);
+	console.log("User flipped " + cardsInPlay[1]);
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert( "Sorry, try again");
+	}
+};
