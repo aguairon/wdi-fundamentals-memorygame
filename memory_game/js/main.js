31e1d945e032b1fb3d5cc64c7ece8b9be0a8dc0e
Begona Fernandez <boadicea1979@yahoo.es>
2018-10-25 10:32:08 +0100

    web dev fundamentals assignment part 9

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -10,8 +31,11 @@ var checkForMatch = function() {
 };
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	var cardFlipped = cards[cardId]
+	console.log("User flipped " + cardFlipped.rank);
+	console.log(cardFlipped.suit);
+	console.log(cardFlipped.cardImage);
+	cardsInPlay.push(cardFlipped.rank);
 
 	if(cardsInPlay.length === 2) {
 		checkForMatch();
